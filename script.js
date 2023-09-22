8fbde3e20fac3b66d984467f9c5baa02b4d76da8
Abdellah-Lahjouji-Drissi <abdellah.lahjouji.drissi@gmail.com>
2023-09-22 23:56:34 +0100

    Update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const navMenu = document.getElementById("nav-menu");
 let navToggle = document.getElementById("nav-toggle");
 let navClose = document.getElementById("nav-close");
 const navLinks = document.querySelectorAll(".nav-link");
+const header = document.getElementById("header");
 
 if(navToggle){
     navToggle.addEventListener( "click" , ()=> {
@@ -21,4 +22,9 @@ navLinks.forEach((link) => {
     link.addEventListener("click" , ()=>{
         navMenu.classList.remove("display-nav");
     })
+})
+
+window.addEventListener("scroll" , () => {
+    this.scrollY >=50 ? header.classList.add("blur-header") : header.classList.remove("blur-header");
+
 })
